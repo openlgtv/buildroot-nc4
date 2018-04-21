a7e596049bc8f73788d429e3ecc1c17bd37fb328
smx-smx <smxdev4@gmail.com>
2018-04-22 01:21:18 +0200

    sdl2_tests: refactored install commands

--- package/sdl2_tests/sdl2_tests.mk
+++ package/sdl2_tests/sdl2_tests.mk
@@ -9,7 +9,7 @@ SDL2_TESTS_SOURCE = SDL2-$(SDL2_TESTS_VERSION).tar.gz
 SDL2_TESTS_SITE = http://www.libsdl.org/release
 SDL2_TESTS_LICENSE = zlib
 SDL2_TESTS_LICENSE_FILES = COPYING
-SDL2_TESTS_INSTALL_STAGING = YES
+SDL2_TESTS_INSTALL_STAGING = NO
 SDL2_TESTS_SUBDIR = test
 
 #SDL2_CONF_OPTS +=
@@ -22,27 +22,23 @@ else
 SDL2_TESTS_CONF_OPTS += --disable-video-directfb
 endif
 
-
 # We must enable static build to get compilation successful.
 SDL2_TESTS_CONF_OPTS += --enable-static
-
-define SDL2_TESTS_CONFIGURE_CMDS
-	(cd $(@D)/$(SDL2_TESTS_SUBDIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		./configure \
-		$(SDL2_TESTS_CONF_OPTS) \
-	)
-endef
 
 define SDL2_TESTS_BUILD_CMDS
 	$(MAKE) -C $(@D)/$(SDL2_TESTS_SUBDIR)
 endef
 
 define SDL2_TESTS_INSTALL_TARGET_CMDS
-	find $(@D)/$(SDL2_TESTS_SUBDIR) -maxdepth 1 -type f -perm /a+x -exec cp {} $(TARGET_DIR)/usr/bin/ \;
 	mkdir -p $(TARGET_DIR)/usr/share/sdl2-tests
+
+	find $(@D)/$(SDL2_TESTS_SUBDIR) -type f -print0 |\
+		xargs -0 -n 5 file -i |\
+		grep "application/x-executable" |\
+		cut -d ':' -f1 |\
+		xargs cp -v -t $(TARGET_DIR)/usr/share/sdl2-tests/
+
 	cp -Ra $(@D)/$(SDL2_TESTS_SUBDIR)/shapes $(TARGET_DIR)/usr/share/sdl2-tests
 endef
 
-$(eval $(generic-package))
+$(eval $(autotools-package))
