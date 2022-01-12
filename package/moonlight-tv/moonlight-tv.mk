95484f723a05bd21ec7cf0118a4959658b94e337
Mariotaku Lee <mariotaku.lee@gmail.com>
2022-01-12 13:26:31 +0900

    updated moonlight

--- package/moonlight-tv/moonlight-tv.mk
+++ package/moonlight-tv/moonlight-tv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MOONLIGHT_TV_VERSION = v1.3.0-1
+MOONLIGHT_TV_VERSION = v1.3.1
 MOONLIGHT_TV_SITE = https://github.com/mariotaku/moonlight-tv.git
 MOONLIGHT_TV_SITE_METHOD = git
 MOONLIGHT_TV_GIT_SUBMODULES = YES
@@ -20,10 +20,10 @@ MOONLIGHT_TV_DEPENDENCIES = \
 	libcurl \
 	expat \
 	freetype fontconfig \
-	json-c pulseaudio \
+	pulseaudio \
 	libegl \
 	umediaserver
-	
+
 MOONLIGHT_TV_CONF_OPTS += \
 	-DTARGET_WEBOS=ON
 
