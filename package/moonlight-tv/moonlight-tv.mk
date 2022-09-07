66cbde348063e1e2627dd660816311946a7d9b4c
Guillaume Smaha <guillaume.smaha@gmail.com>
2022-09-07 21:50:31 +0200

    Fix broken dependency message_queue for moonlight-tv
    Use "git config --global" to replace the broken dependency by a reupload
    of the code.

--- package/moonlight-tv/moonlight-tv.mk
+++ package/moonlight-tv/moonlight-tv.mk
@@ -9,6 +9,11 @@ MOONLIGHT_TV_SITE = https://github.com/mariotaku/moonlight-tv.git
 MOONLIGHT_TV_SITE_METHOD = git
 MOONLIGHT_TV_GIT_SUBMODULES = YES
 
+define MOONLIGHT_TV_PATCH_SUBMODULE
+  git config --global url."https://github.com/GuillaumeSmaha/message_queue".insteadOf "https://github.com/LnxPrgr3/message_queue"
+endef
+MOONLIGHT_TV_PRE_DOWNLOAD_HOOKS += MOONLIGHT_TV_PATCH_SUBMODULE
+
 MOONLIGHT_TV_DEPENDENCIES = \
 	host-pkgconf \
 	libglib2 \
