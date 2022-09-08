e6902a18c818a2affe1b5a5adacf8a45a9d3037d
Guillaume Smaha <guillaume.smaha@gmail.com>
2022-09-08 18:53:11 +0200

    Upgrade moonlight-tv to 1.5.2
    Enable host-nodejs to run moonlight-tv scripts during build

--- package/moonlight-tv/moonlight-tv.mk
+++ package/moonlight-tv/moonlight-tv.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-MOONLIGHT_TV_VERSION = v1.3.1
+MOONLIGHT_TV_VERSION = v1.5.2
 MOONLIGHT_TV_SITE = https://github.com/mariotaku/moonlight-tv.git
 MOONLIGHT_TV_SITE_METHOD = git
 MOONLIGHT_TV_GIT_SUBMODULES = YES
 
 MOONLIGHT_TV_DEPENDENCIES = \
+	host-nodejs \
 	host-pkgconf \
 	libglib2 \
 	libpbnjson \
