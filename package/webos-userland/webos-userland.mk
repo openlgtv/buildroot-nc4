a0df7fd6d4780e32f07223e91f417921f9fcc594
Stephan Sundermann <s.sundermann@fu-berlin.de>
2023-02-05 13:03:56 +0100

    Add webos-userland

---
+++ package/webos-userland/webos-userland.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# webos-userland
+#
+################################################################################
+
+WEBOS_USERLAND_VERSION = ea82c15c4fcf655cc231ab57503804af6f5f793f
+WEBOS_USERLAND_SITE = $(call github,sundermann,webos-userland,$(WEBOS_USERLAND_VERSION))
+WEBOS_USERLAND_LICENSE = Apache/MIT
+WEBOS_USERLAND_INSTALL_STAGING = YES
+
+WEBOS_USERLAND_PROVIDES = libegl libgles
+
+WEBOS_USERLAND_POST_INSTALL_TARGET_HOOKS += WEBOS_USERLAND_EXTRA_LIBS_TARGET
+
+WEBOS_USERLAND_POST_INSTALL_STAGING_HOOKS += WEBOS_USERLAND_EXTRA_LIBS_STAGING
+
+define WEBOS_USERLAND_POST_TARGET_CLEANUP
+	rm -Rf $(TARGET_DIR)/usr/src
+endef
+WEBOS_USERLAND_POST_INSTALL_TARGET_HOOKS += WEBOS_USERLAND_POST_TARGET_CLEANUP
+
+$(eval $(cmake-package))
