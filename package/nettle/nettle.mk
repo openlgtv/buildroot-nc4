4a207705d7757fc4ce8fc2a70cde8a2a4c619cc9
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2024-01-31 18:42:30 -0500

    Revert "nettle: bump to version 3.1.1"
    This reverts commit 8eaafb78ad7925e37c51b2aeb968973abd35b18d.
    
    Preparing to switch to older versions of gmp and gnutls.

--- package/nettle/nettle.mk
+++ package/nettle/nettle.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-NETTLE_VERSION = 3.8.1
-NETTLE_SITE = https://ftp.gnu.org/gnu/nettle
+NETTLE_VERSION = 2.7.1
+NETTLE_SITE = http://www.lysator.liu.se/~nisse/archive
 NETTLE_DEPENDENCIES = gmp
 NETTLE_INSTALL_STAGING = YES
-NETTLE_LICENSE = Dual GPL-2.0+/LGPL-3.0+
-NETTLE_LICENSE_FILES = COPYING.LESSERv3 COPYINGv2
-NETTLE_CPE_ID_VENDOR = nettle_project
+NETTLE_LICENSE = LGPLv2.1+
+NETTLE_LICENSE_FILES = COPYING.LIB
 # don't include openssl support for (unused) examples as it has problems
 # with static linking
 NETTLE_CONF_OPTS = --disable-openssl
@@ -20,11 +19,18 @@ ifeq ($(BR2_ARM_CPU_ARMV4)$(BR2_ARM_CPU_ARMV5)$(BR2_ARM_CPU_ARMV7M),y)
 NETTLE_CONF_OPTS += --disable-assembler
 endif
 
-ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
+# ARM NEON, requires binutils 2.21+
+ifeq ($(BR2_ARM_CPU_HAS_NEON)$(BR2_TOOLCHAIN_BUILDROOT)$(BR2_BINUTILS_VERSION_2_20_1),yy)
 NETTLE_CONF_OPTS += --enable-arm-neon
 else
 NETTLE_CONF_OPTS += --disable-arm-neon
 endif
+
+define NETTLE_DITCH_DEBUGGING_CFLAGS
+	$(SED) '/CFLAGS/ s/ -ggdb3//' $(@D)/configure
+endef
+
+NETTLE_POST_EXTRACT_HOOKS += NETTLE_DITCH_DEBUGGING_CFLAGS
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
