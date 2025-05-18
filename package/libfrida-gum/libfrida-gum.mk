a35f05fc18a4bed8c3ec368b1be746011afe6a9d
Stefano Moioli <smxdev4@gmail.com>
2025-05-19 00:01:55 +0200

    fix libfrida-gum build - switch to internally managed dependencies - use frida's fork of libcapstone - add libdwarf

--- package/libfrida-gum/libfrida-gum.mk
+++ package/libfrida-gum/libfrida-gum.mk
@@ -4,42 +4,35 @@
 #
 ################################################################################
 
-LIBFRIDA_GUM_VERSION = main
+LIBFRIDA_GUM_VERSION = 0c8df6742d2c7a08c32fc3c3b691779b062fb34a
 LIBFRIDA_GUM_SITE = https://github.com/frida/frida-gum
 LIBFRIDA_GUM_SITE_METHOD = git
-LIBFRIDA_GUM_GIT_SUBMODULES = YES
+LIBFRIDA_GUM_GIT_SUBMODULES = NO
 
-#LIBFRIDA_GUM_DEPENDENCIES =
+LIBFRIDA_GUM_DEPENDENCIES = host-libglib2 libglib2 \
+	libunwind libffi libcapstone \
+	libdwarf
 
 ifeq ($(BR2_PACKAGE_LIBFRIDA_GUM_DIET),y)
 LIBFRIDA_GUM_CONF_OPTS += -Ddiet=true -Db_ndebug=true
 endif
-
-# force frida dependency variants
-LIBFRIDA_GUM_CONF_OPTS += --wrap-mode=forcefallback
 
 LIBFRIDA_GUM_CFLAGS = $(TARGET_CFLAGS)
 LIBFRIDA_GUM_LDFLAGS = $(TARGET_LDFLAGS) $(TARGET_NLS_LIBS)
 
 LIBFRIDA_GUM_CFLAGS += \
 	-ffunction-sections \
-	-fdata-sections
+	-fdata-sections \
+	-fPIC
+
 LIBFRIDA_GUM_LDFLAGS += -Wl,--gc-sections
 
 LIBFRIDA_GUM_CONF_OPTS += \
 	-Dgumpp=disabled \
 	-Dgumjs=disabled \
-	-Dtests=disabled \
-	-Db_staticpic=true
+	-Dtests=disabled
 
 LIBFRIDA_GUM_INSTALL_STAGING = YES
 LIBFRIDA_GUM_INSTALL_TARGET = NO
 
-# HACK: force static build
-ifeq ($(BR2_STATIC_LIBS),)
-BR2_STATIC_LIBS=y
 $(eval $(meson-package))
-BR2_STATIC_LIBS=
-else
-$(eval $(meson-package))
-endif
