5796a567ab022ea1a802c915036beaca9a62dd4a
Stefano Moioli <smxdev4@gmail.com>
2025-05-18 20:39:49 +0200

    update glibc fix build deadlock/endless loop in make rules

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -18,7 +18,7 @@ else
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
 #GLIBC_VERSION = 2.35-134-gb6aade18a7e5719c942aa2da6cf3157aca993fa4
-GLIBC_VERSION = 22b856de12dcb02dfca6251012989befc4138b2d
+GLIBC_VERSION = 43154dcd317e74de4146eaa6767eaee770c9cadc
 
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
